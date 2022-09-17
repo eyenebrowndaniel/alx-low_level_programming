f709d82b76161caff0c26de512fbc5e8beb858d7
eyenebrowndaniel <danieleyenebrown@gmail.com>
2022-09-17 21:55:40 +0100

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include "holberton.h"
+#include "_putchar.c"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
-}[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[3~_[p[u[t[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[B[3~[[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C_putchar[3~[[A[A[A[D[D[D[D[D[D_putchar[3~[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B
+}
